8a94e3e99837949fb97d870e0bba07e4eea8f029
Suzette <suzetteislam@gmail.com>
2020-09-30 23:49:50 -0400

    tip form created

--- src/App.js
+++ src/App.js
@@ -7,10 +7,18 @@ function App() {
     <div className="App">
       <h1>Split Tip</h1>
       <form>
-        <p>How much was your bill?</p> <br>
-        </br>
+        <p>How much was your bill?</p> 
         <input type='number' name ='price' min='0' step='any'/>
+        <p>How was your service?</p>
+        <select>
+          <option>--Select an option--</option>
+        </select>
+        <p>How many people are splitting the tip?</p>
+        <input type='number'/> <span>people</span>
+        <br/>
+        <button>Split the tip!</button>
       </form>
+
     </div>
   );
 }
