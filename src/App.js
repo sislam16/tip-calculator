70736868267d8bf6f5ecc0d69c961ac34ed00516
Suzette <suzetteislam@gmail.com>
2020-09-30 23:10:19 -0400

    working on calculator

--- src/App.js
+++ src/App.js
@@ -5,20 +5,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Split Tip</h1>
+      <form>
+        <p>How much was your bill?</p> <br>
+        </br>
+        <input type='number' name ='price' min='0' step='any'/>
+      </form>
     </div>
   );
 }
